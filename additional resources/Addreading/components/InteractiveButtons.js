92b93fce009ad0db2cf9182cb51b31b0cf2ddcdd
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-03-30 16:09:32 +0200

    Update InteractiveButtons.js

--- additional resources/Addreading/components/InteractiveButtons.js
+++ additional resources/Addreading/components/InteractiveButtons.js
@@ -1,18 +1,27 @@
+// Import necessary icons from the react-icons library
 import { MdModeComment, MdFavoriteBorder } from 'react-icons/md'
+
+// Define the component as a functional component
 export default ({likes, LikesEntry, commentsNum}) => (
     <div className="meta">
+    {/* Like button with the heart icon */}
         <button className="heart" onClick={LikesEntry} ><MdFavoriteBorder />{likes}</button>
+    {/* Paragraph displaying the number of comments with a comment icon */}
         <p><MdModeComment />{ commentsNum }</p>
+            {/* Component-specific styles */}
         <style>{`
+        /* Styles for the metadata container */
         .meta{
             padding: 5px;
             background: var(--light-gray);
             border-top: 1px solid #dce2e4;
             overflow: auto;
         }
+        /* Styles for the paragraph inside the metadata */
         .meta p {
             padding-top: 9px;
         }
+         /* Styles for the like button and paragraph */
         .meta button, .meta p{
             border: none;
             background: transparent;
@@ -25,6 +34,7 @@ export default ({likes, LikesEntry, commentsNum}) => (
             color: var(--green);
             outline: none;
         }
+         /* Styles for the icons inside the metadata */
         .meta svg {
             font-size: 1.3rem;
             margin-right: 3px;
