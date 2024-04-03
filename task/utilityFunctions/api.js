3eb23b11881449b043d73ea626db3250d72236db
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-04-03 11:44:05 +0200

    Delete task directory

--- task/utilityFunctions/api.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'isomorphic-unfetch'
-
-export async function fetchData() {
-    const response = await fetch('https://api.tvmaze.com/search/shows?q=batman')
-    const data = await response.json()
-    return data;
-}
